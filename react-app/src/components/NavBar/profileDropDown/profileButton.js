0f5a265b00933b3235635918d8e7ff7c15c098ee
tyler <tylernaej@gmail.com>
2022-10-05 18:45:08 -0400

    update

--- react-app/src/components/NavBar/profileDropDown/profileButton.js
+++ react-app/src/components/NavBar/profileDropDown/profileButton.js
@@ -39,7 +39,7 @@ function ProfileButton() {
             </div>
         )
         }
-        {!sessionUser && (
+        {/* {!sessionUser && (
             <div className="flex-row"> 
                 <div id='login-container'>
                     <NavLink 
@@ -58,7 +58,7 @@ function ProfileButton() {
                     </NavLink>
                 </div>
             </div>
-        )}
+        )} */}
     </div>
   )
 }

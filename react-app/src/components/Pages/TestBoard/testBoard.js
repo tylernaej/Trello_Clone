833ec0cf7c37ac3ab073f6ad17ecfa97bc4c9a76
tyler <tylernaej@gmail.com>
2023-04-30 17:43:36 -0400

    working test code

--- react-app/src/components/Pages/TestBoard/testBoard.js
+++ react-app/src/components/Pages/TestBoard/testBoard.js
@@ -25,9 +25,9 @@ const onDragEnd = (result, columns, setColumns) => {
     const {source, destination} = result
     if (source.droppableId !== destination.droppableId) {
         const sourceColumn = columns[source.droppableId];
-        const destColumn = columns[destination.droppableId]
-        const sourceItems = [sourceColumn.items];
-        const destItems = [destColumn.items];
+        const destColumn = columns[destination.droppableId];
+        const sourceItems = [...sourceColumn.items];
+        const destItems = [...destColumn.items];
         const [removed] = sourceItems.splice(source.index, 1);
         destItems.splice(destination.index, 0, removed);
         setColumns({
